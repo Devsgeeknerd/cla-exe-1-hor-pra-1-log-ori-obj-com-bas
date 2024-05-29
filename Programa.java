ea74f581c7ccf9698c6ef0d3fb59dda1854f2b6d
Paulo Vanderley <vanderley.1109@gmail.com>
2024-05-29 16:59:35 -0300

    :page_with_curl: Update file content.

--- Programa.java
+++ Programa.java
@@ -1,6 +1,9 @@
 package br.com.devsgeeknerd.consultorio;
 
+import br.com.devsgeeknerd.consultorio.Dentista;
+
 public class Programa {
   public static void programa(String[] args) {
+    Dentista dentista1 = new Dentista("Paulo", "Geek", "Ortodontia", "CRO135799", true);
   }
 }
